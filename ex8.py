c38560a91b530ac5de32899f64b62c7eb6bf4ae2
Shaban <shabanxhemajli@live.com>
2022-08-24 13:38:40 +0200

    hhh

--- ex8.py
+++ ex8.py
@@ -1,3 +1,8 @@
 #Task1
 text="Berlin is a world city of culture, politics, media and science."
 print(len(text))
+#Task2
+text="Berlin is a world city of culture, politics, media and science."
+first_char=text[0]
+last_char=text[-1]
+print(first_char + last_char)
