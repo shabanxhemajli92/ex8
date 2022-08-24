3a1ca0dec05b57c49f2b4639f70d31d8cf235377
Shaban <shabanxhemajli@live.com>
2022-08-24 13:51:49 +0200

    vv

--- ex8.py
+++ ex8.py
@@ -6,7 +6,11 @@ text="Berlin is a world city of culture, politics, media and science."
 first_char=text[0]
 last_char=text[-1]
 print(first_char + last_char)
-#Task2
+#Task3
 text="Berlin is a world city of culture, politics, media and science."
 first_three=text[0:3]
 print(first_three.upper())
+#Task4
+text="Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital"
+b_appears=text.count("b")
+print("B appears: " + str(b_appears) + "" ,"times")
